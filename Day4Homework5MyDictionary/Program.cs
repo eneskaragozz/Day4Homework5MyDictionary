8878753040b4c2c69a0cc6fdba40633314e9ae63
Enes KARAGÖZ <eneskaragoz44@gmail.com>
2021-02-27 17:35:06 +0300

    Add project files.

---
+++ Day4Homework5MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4Homework5MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> fruits = new Dictionary<string, string>();
+            fruits.Add("Malatya", "Kayısı");
+            fruits.Add("Amasya", "Elma");
+            fruits.Add("Diyarbakır", "Karpuz");
+            
+        }
+    }
+}
